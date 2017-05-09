e2bd5c7b99222449ef2f6ce8709bc5e033ab5ecb
Muskan Shrestha <musk0047@edu.easj.dk>
2017-05-09 10:06:38 +0200

    create

---
+++ WcfRestParking/IParkingService1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfRestParking
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IParkingService1" in both code and config file together.
+    [ServiceContract]
+    public interface IParkingService1
+    {
+
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    
+}
---
+++ WcfRestParking/ParkingService1.svc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfRestParking
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ParkingParkingService1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ParkingParkingService1.svc or ParkingParkingService1.svc.cs at the Solution Explorer and start debugging.
+    public class ParkingParkingService1 : IParkingService1
+    {
+        
+    }
+}
