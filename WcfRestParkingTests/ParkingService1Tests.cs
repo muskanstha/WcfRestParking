2c4d1d16ca1f5dee6a68b2d7630f25d7b761bdf1
Deepak Gautam <deep0087@edu.easj.dk>
2017-05-18 14:52:26 +0200

    D3

---
+++ WcfRestParkingTests/ParkingService1Tests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WcfRestParking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfRestParking.Tests
+{
+    [TestClass()]
+    public class ParkingService1Tests
+    {
+        private ParkingService1 s= new ParkingService1();
+        [TestMethod()]
+        public void GetStatusesTest()
+        {
+            Assert.AreEqual(6,s.GetStatuses().Count);
+        }
+    }
+}
