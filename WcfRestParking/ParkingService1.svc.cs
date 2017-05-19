871f9bf8d749116f12b4861af6463acb8f54fbdb
Muskan Shrestha <musk0047@edu.easj.dk>
2017-05-20 00:11:06 +0200

    fsdgs

--- WcfRestParking/ParkingService1.svc.cs
+++ WcfRestParking/ParkingService1.svc.cs
@@ -134,8 +134,7 @@ namespace WcfRestParking
         {
             using (HttpClient client = new HttpClient())
             {
-                string uri =
-                    "https://query.yahooapis.com/v1/public/yql?q=select%20item.condition.text%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22dallas%2C%20tx%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+                string uri = "https://query.yahooapis.com/v1/public/yql?q=select%20item.condition.text%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22roskilde%2C%20dk%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
                 string content = client.GetStringAsync(uri).Result;
                 JObject ajJObject = JObject.Parse(content);
                 //string theText = ajJObject.query.k
