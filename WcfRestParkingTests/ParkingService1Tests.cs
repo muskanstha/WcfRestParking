e647f7517257a9da2cd5a335161434a6791bc005
Deepak Gautam <deep0087@edu.easj.dk>
2017-05-19 13:11:10 +0200

    deepak

--- WcfRestParkingTests/ParkingService1Tests.cs
+++ WcfRestParkingTests/ParkingService1Tests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WcfRestParking.Models;
 
 namespace WcfRestParking.Tests
 {
@@ -16,6 +17,27 @@ namespace WcfRestParking.Tests
         public void GetStatusesTest()
         {
             Assert.AreEqual(6,s.GetStatuses().Count);
+        }
+
+        [TestMethod()]
+        public void CreateStatus()
+        {
+            Status aStatus= new Status(){Distance = 2,SpotNo = "3A",IsFree = "No"};
+            int size = s.GetStatuses().Count;
+            s.CreateStatus(aStatus);
+            Assert.AreEqual(size+1,s.GetStatuses().Count);
+
+        }
+        [TestMethod]
+        public void ChangeStatus()
+        {
+            
+            var aStatus = new Status(){SpotNo = "2A", Distance=3};
+            Status ups = s.GetStatuses().Last();
+            ups.IsFree = "Yes";
+            s.ChangeStatus(aStatus);
+            Assert.Equals(ups, aStatus);
+
         }
     }
 }
