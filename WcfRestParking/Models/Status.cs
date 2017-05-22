0fabf27a4090f385551ec9d841d8d32c706b8772
Muskan Shrestha <musk0047@edu.easj.dk>
2017-05-22 12:45:50 +0200

    sadf

--- WcfRestParking/Models/Status.cs
+++ WcfRestParking/Models/Status.cs
@@ -6,6 +6,7 @@ using System.Web;
 namespace WcfRestParking.Models
 {
     /// <summary>
+    /// jesper
     /// CLass for the log table in database with matching data type
     /// </summary>
     public class Status
@@ -13,6 +14,9 @@ namespace WcfRestParking.Models
         public string SpotNo { get; set; }
         public string IsFree { get; set; }
         public decimal Distance { get; set; }
+        /// <summary>
+        /// By: bipin . prevent null value
+        /// </summary>
         public string spotNO
         {
             get { return SpotNo; }
@@ -22,7 +26,12 @@ namespace WcfRestParking.Models
                 SpotNo = value;
             }
         }
-
+        /// <summary>
+        /// constructor with parameters
+        /// </summary>
+        /// <param name="spotNo"></param>
+        /// <param name="isFree"></param>
+        /// <param name="distance"></param>
         public Status(string spotNo, string isFree, decimal distance)
         {
             SpotNo = spotNo;
@@ -30,6 +39,9 @@ namespace WcfRestParking.Models
             Distance = distance;
         }
 
+        /// <summary>
+        /// default constructor
+        /// </summary>
         public Status()
         {
             
